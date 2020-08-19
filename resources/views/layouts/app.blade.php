81ff46f9df3ca9612bf2b7626b840f16c29fcd32
Erkin <mackinkenny@gmail.com>
2020-08-19 19:24:16 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Jizelle</title>
-    <link rel="icon" type="image/png" href="{{ asset('image/logo.svg') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.svg') }}">
 
     <!-- Scripts -->
 
