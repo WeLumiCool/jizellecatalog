aaf305d69afc7c5c3c5a2b878e2e03b7530a2a48
Erkin <mackinkenny@gmail.com>
2020-08-19 19:13:40 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Jizelle</title>
+    <link rel="icon" type="image/png" href="{{ asset('image/logo.svg') }}">
 
     <!-- Scripts -->
 
