9972ecaa9b4b710535563a7028f63a28931c95d5
Erkin <mackinkenny@gmail.com>
2020-08-20 14:22:22 +0600

    model carousel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
 </head>
 <body>
     <div id="app">
@@ -42,13 +44,24 @@
                     <button type="button" class="close position-absolute" style="right:5%; top:5%; z-index:9999;" data-dismiss="modal" aria-label="Close">
                         <img src="{{ asset('images/close.svg') }}" alt="">
                     </button>
-                    <div class="modal-body text-center" style="padding: 40px;">
-                        <div class="px-5">
-                            <img class="img-fluid" src="{{ asset('storage/'.json_decode($product->image)[0]) }}" alt="">
+                    <div class="modal-body text-center p-0">
+                        <div class="px-lg-0 px-0 owl-one owl-carousel text-center">
+                            @foreach(json_decode($product->image) as $image)
+                                <div class="item">
+                                    <img class="w-100" src="{{ asset('storage/'.$image) }}" alt="">
+                                </div>
+                            @endforeach
+                            {{--<div class="item">--}}
+                                {{--<img class="w-100" src="{{ asset('storage/'.json_decode($product->image)[0]) }}" alt="">--}}
+                            {{--</div>--}}
+                            {{--<div class="item">--}}
+                                {{--<img class="w-100" src="{{ asset('storage/'.json_decode($product->image)[1]) }}" alt="">--}}
+                            {{--</div>--}}
                         </div>
-                        <div class="row">
-                            <div class="col-9">
-                                <div class="text-left px-5 mt-4">
+                        <div class="container">
+                        <div class="d-flex">
+                            <div class="col-8">
+                                <div class="text-left px-lg-5 px-1 mt-4">
                                     <p class="font-size-12 text-secondary font-weight-light mb-1">{{$product->article}}</p>
                                     <p class="font-size-18 text-dark mb-1">{{ $product->title }}</p>
                                     <div class="d-flex">
@@ -58,19 +71,20 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-3 py-5">
+                            <div class="col-4 py-5 px-lg-3 px-0">
                                 <div class="d-flex align-items-center justify-content-center add-to-cart mt-3 py-2" data-id="{{ $product->id }}" style="width: 80px; background: #2F2F2F; cursor: pointer; transition: 0.5s">
                                     <img src="{{ asset('images/addcart.svg') }}" alt="">
                                 </div>
                             </div>
                         </div>
-
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     @endforeach
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/owl.carousel.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-173937125-2"></script>
@@ -142,6 +156,22 @@
 
             console.log(btn.data('id'));
         });
+    </script>
+    <script>
+        var owl = $('.owl-one');
+        owl.owlCarousel({
+            margin: 10,
+            loop: true,
+            // autoplay:true,
+            // autoplayTimeout:5000,
+            // autoplaySpeed: 1500,
+            // autoplayHoverPause:true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+            }
+        })
     </script>
 </body>
 </html>
