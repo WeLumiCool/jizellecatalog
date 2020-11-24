835ea0353ae8a2bd62acf393c66a5522912df96b
Erkin <mackinkenny@gmail.com>
2020-11-24 13:00:30 +0600

    Update single.blade.php

--- resources/views/catalog/single.blade.php
+++ resources/views/catalog/single.blade.php
@@ -286,7 +286,7 @@
             </div>
             @foreach($prods as $prod)
             <div class="col-lg-3 col-6 mt-lg-0 mt-4">
-                <a href="{{ route('product/show', $product->id) }}" style="text-decoration: none;">
+                <a href="{{ route('product/show', $prod->id) }}" style="text-decoration: none;">
                 <div class="shadow-sm h-100">
                     <img class="w-100 mb-2" src="{{ asset('storage/'.json_decode($prod->image)[0]) }}" alt="">
                     <div class="p-3">
